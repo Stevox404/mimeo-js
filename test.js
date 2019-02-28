f3d4a3fddc0bb3dced18a93a1d411e583f6a1482
Benz Stevox <benzstevox@windows>
2019-02-28 16:49:57 +0300

    Proper tests added

--- test.js
+++ test.js
@@ -1,24 +1,69 @@
+process.env.NODE_ENV = 'test';
+
 const mimic = require('./index.js').mimic;
+const chai = require('chai');
+const expect = chai.expect;
 
-console.log("Get random 'similar' data.\n\n");
-
-console.log('Get random string:',mimic('hippopotumus'));
-console.log('Get random sentence:',mimic('Hello there World!'));
-console.log('Choose from list [A-E]:',mimic(() => ['A', 'B', 'C', 'D', 'E']));
-console.log('Get random number:',mimic(274));
-console.log('Get random double:',mimic(-27.5664, true));
-console.log('Get random boolean:',mimic(true));
-console.log('Get random array:',mimic([1, 2, [345, 6, 23], 40]));
-console.log('Get random object:',mimic(
-    { 
-        email: 'foo@bar.com', 
-        username: 'foo404',
-        role: () => ['administrator', 'moderator', 'user'], 
-        details: { 
-            gender: 'male', 
-            dob: new Date(), 
-        } 
-    }
-));
-
-//TODO: Write proper tests
+describe('Test:', () => {
+    it('Should get a random string', (done) => {
+        const rnd = mimic('hippopotumus');
+        expect(rnd).to.be.a('string');
+        done();
+    });
+    it('Should get a random sentence', (done) => {
+        const rnd = mimic('Hello there World!');
+        const numWords = rnd.split(' ').length;
+        expect(rnd).to.be.a('string');
+        expect(numWords).to.be.greaterThan(1);
+        done();
+    });
+    it('Should choose from list', (done) => {
+        const rnd = mimic(() => ['A', 'B', 'C', 'D', 'E']);
+        expect(rnd).to.be.oneOf(['A', 'B', 'C', 'D', 'E']);
+        done();
+    });
+    it('Should get a random number', (done) => {
+        const rnd = mimic(274);
+        expect(rnd).to.be.a('number')
+        done();
+    });
+    it('Should get a random double', (done) => {
+        const rnd = mimic(3.14);
+        expect(rnd).to.be.a('number')
+        done();
+    });
+    it('Should get a random boolean', (done) => {
+        const rnd = mimic(true);
+        expect(rnd).to.be.a('boolean')
+        done();
+    });
+    it('Should get a random array', (done) => {
+        const rnd = mimic([1, 2, [345, 6, 23], 40]);
+        expect(rnd).to.be.an('array')
+        expect(rnd[0]).to.be.a('number')
+        expect(rnd[2]).to.be.an('array')
+        done();
+    });
+    it('Should get a random object', (done) => {
+        const rnd = mimic(
+            { 
+                email: 'foo@bar.com', 
+                username: 'foo404',
+                role: () => ['administrator', 'moderator', 'user'], 
+                details: { 
+                    gender: 'male', 
+                    dob: new Date(), 
+                } 
+            }
+        );
+        expect(rnd).to.be.an('Object')
+        expect(rnd).to.have.property('email').which.is.a('string');
+        expect(rnd).to.have.property('username').which.is.a('string');
+        expect(rnd).to.have.property('role').which.is.oneOf(['administrator', 'moderator', 'user']);
+        expect(rnd).to.have.property('details').which.is.an('Object')
+            .with.property('gender').which.is.a('string');
+        expect(rnd).to.have.property('details').which.is.an('Object')
+            .with.property('dob').which.is.a('Date');
+        done();
+    });
+});
