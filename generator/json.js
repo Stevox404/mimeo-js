92659f3b4fe79dc38b0168f823b5fc0594d5b2ab
Benz Stevox <benzstevox@windows>
2019-02-28 16:53:16 +0300

    Better type checking for JSON values

--- generator/json.js
+++ generator/json.js
@@ -8,27 +8,27 @@ function guessKeys(key, val, seed){
             return Chance(seed && val).pickone(data);
         }
     }
-    if(/email/i.test(key)){
+    if(/email/i.test(key) && /.+?@.+\..+/.test(val)){
         return Chance(seed && val).email({domain: 'mailinator.com'});
     }
-    if(/firstName|fName/i.test(key)){
+    if(/firstName|fName/i.test(key) && typeof val === 'string'){
         return Chance(seed && val).first();
     }
-    if(/name/i.test(key)){
+    if(/name/i.test(key) && typeof val === 'string'){
         if(val.split(' ').length>1){
             return Chance(seed && val).name();
         } else {
             return Chance(seed && val).last();
         }
     }
-    if(/gender/i.test(key)){
+    if(/gender/i.test(key) && typeof val === 'string'){
         const gen = Chance(seed && val).gender();
         return val.length > 1 ? gen : gen[0];
     }
     if(/d\.?o\.?b|date|birthday/i.test(key) && val instanceof Date){
         return Chance(seed && val).birthday();
     }
-    if(/city|town/i.test(key)){
+    if(/city|town/i.test(key) && typeof val === 'string'){
         return Chance(seed && val).city();
     }
     
